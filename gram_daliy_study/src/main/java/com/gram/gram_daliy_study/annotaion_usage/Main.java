93a711ecbd2df5b2d427b545170e02c657c5a9b3
zotij <zotij@outlook.com>
2024-03-31 01:32:25 +0800

    自定义注解的使用;

---
+++ gram_daliy_study/src/main/java/com/gram/gram_daliy_study/annotaion_usage/Main.java
@@ -0,0 +1,41 @@
+package com.gram.gram_daliy_study.annotaion_usage;
+
+import com.gram.gram_daliy_study.annotaion_usage.Vo.Student;
+import com.gram.gram_daliy_study.annotaion_usage.annotation.MyAnnotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
+/**
+ * @Author:zotij
+ * @Date: 2024/3/30  15:32
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Class<?> studentClass = Student.class;
+        Field[] fields = studentClass.getDeclaredFields();
+        for (Field field: fields) {
+            String fieldName = field.getName();  // 获取该类成员变量的名字
+            System.out.println("成员变量的名字是：" + fieldName);
+            Annotation[] annotations = field.getAnnotations();
+            for (Annotation annotation : annotations) {
+                Class<? extends Annotation> annatationType = annotation.annotationType();
+                String annatationName = annatationType.getSimpleName();   // 注解的简短名称
+                System.out.println("使用的注解是：" + annatationName);
+
+                // 判断该注解是不是MyAnnotation 注解， 是的话将其打印 id 和 describe 属性
+                if (annatationType.equals(MyAnnotation.class)) {
+                    MyAnnotation myAnnotation = field.getAnnotation(MyAnnotation.class);
+                    int id = myAnnotation.id();
+                    String describe = myAnnotation.describe();
+                    System.out.println("MyAnnotaion注解中的id是：" + id);
+                    System.out.println("My Annotation注解中的describe是：" + describe);
+
+                }
+
+            }
+        }
+        
+    }
+}
---
+++ gram_daliy_study/src/main/java/com/gram/gram_daliy_study/annotaion_usage/Vo/Student.java
@@ -0,0 +1,38 @@
+package com.gram.gram_daliy_study.annotaion_usage.Vo;
+
+import com.alibaba.fastjson2.annotation.JSONField;
+import com.gram.gram_daliy_study.annotaion_usage.annotation.MyAnnotation;
+
+/**
+ * @Author:zotij
+ * @Date: 2024/3/30  15:08
+ */
+public class Student {
+
+    @JSONField(ordinal =0)
+    @MyAnnotation
+    public String name;
+
+    @MyAnnotation(id=1,describe="分数")
+    public Integer score;
+
+    public Student() {
+    }
+
+    @MyAnnotation(describe="getName方法")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+}
