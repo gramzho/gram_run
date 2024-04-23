94cb9fb4af88edebc5009953d10e5044e2b27d8f
zotij <zotij@outlook.com>
2024-04-23 23:46:48 +0800

    模块名字修改;

--- gram_daily_study/src/test/java/com/gram/gram_daily_study/Appliation.java
+++ gram_daily_study/src/test/java/com/gram/gram_daily_study/Appliation.java
@@ -1,6 +1,7 @@
 package com.gram.gram_daily_study;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
